db2f2766bde016dc0d842dae76aa958e3e13ec79
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-26 12:57:11 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,8 +9,8 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
   end
 end
  
