48d8f054ab8dab7c98a573f1cbf2b9a22fd9b587
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-26 13:19:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if birthday_kids 
+    if age < 27 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
